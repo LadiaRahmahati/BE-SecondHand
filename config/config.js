a646988beb6e750601135f17755c393f63979f5b
Ladia Rahmahati <ladiarahmahati@students.unnes.ac.id>
2022-07-24 21:47:53 +0700

    .

--- config/config.js
+++ config/config.js
@@ -2,24 +2,42 @@ require('dotenv').config();
 
 module.exports = {
   "development": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    "username": "pzokeoxgrnivli",
+    "password": "bc9a6a2943a7d0d353f836c49d4ca6a17bc9d4e06530f21d826f439a5836d457",
+    "database": "dd7dvffbhgjmd6",
+    "host": "ec2-100-26-39-41.compute-1.amazonaws.com",
+    dialect: "postgres",
+    "dialectOptions": {
+      "ssl": {
+        "require": true,
+        "rejectUnauthorized": false
+      }
+    }
   },
   "test": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    "username": "pzokeoxgrnivli",
+    "password": "bc9a6a2943a7d0d353f836c49d4ca6a17bc9d4e06530f21d826f439a5836d457",
+    "database": "dd7dvffbhgjmd6",
+    "host": "ec2-100-26-39-41.compute-1.amazonaws.com",
+    dialect: "postgres",
+    "dialectOptions": {
+      "ssl": {
+        "require": true,
+        "rejectUnauthorized": false
+      }
+    }
   },
   "production": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-  }
+    "username": "pzokeoxgrnivli",
+    "password": "bc9a6a2943a7d0d353f836c49d4ca6a17bc9d4e06530f21d826f439a5836d457",
+    "database": "dd7dvffbhgjmd6",
+    "host": "ec2-100-26-39-41.compute-1.amazonaws.com",
+    dialect: "postgres",
+    "dialectOptions": {
+      "ssl": {
+        "require": true,
+        "rejectUnauthorized": false
+      }
+    }
+  },
 }
